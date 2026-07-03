5f1c628bc2a9cfe3673ed8e9cdac659757d98154
roshnn-sahu <locroshan@gmail.com>
2026-07-03 19:15:28 +0530

    feat: implement magnetic button component with spring-based mouse interaction

--- magnatic-button/app/page.tsx
+++ magnatic-button/app/page.tsx
@@ -3,7 +3,7 @@ import MagneticButton from "@/components/magnetic-button";
 
 const page = () => {
   return (
-    <div>
+    <div className="flex h-screen w-full items-center justify-center">
       <MagneticButton />
     </div>
   );
